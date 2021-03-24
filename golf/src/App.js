e89fa837fec4ff3204452933d237ab0a7ffc1d88
therjmurray <therjmurray@gmail.com>
2021-03-23 20:07:17 -0400

    defence to shield

--- golf/src/App.js
+++ golf/src/App.js
@@ -14,8 +14,10 @@ const App = () => {
 	const [player2Attack, setPlayer2Attack] = useState();
 	const [player1Health, setPlayer1Health] = useState(1000);
 	const [player2Health, setPlayer2Health] = useState(1000);
-	const [player1Defence, setPlayer1Defence] = useState(0);
-	const [player2Defence, setPlayer2Defence] = useState(0);
+	// const [player1Defence, setPlayer1Defence] = useState(0);
+	// const [player2Defence, setPlayer2Defence] = useState(0);
+	const [player1Shield, setPlayer1Shield] = useState(0);
+	const [player2Shield, setPlayer2Shield] = useState(0);
 	const [player1Debuffs, setPlayer1Debuffs] = useState([])
 	const [player2Debuffs, setPlayer2Debuffs] = useState([])
 	const [result, setResult] = useState();
@@ -96,8 +98,8 @@ const App = () => {
 			}else{
 				//Continue the battle
 
-				p1Shield = player1Defence + p1Update.shield;
-				p2Shield = player2Defence + p2Update.shield;
+				p1Shield = player1Shield + p1Update.shield;
+				p2Shield = player2Shield + p2Update.shield;
 
 					//Player 1
 					if(p1Shield > 0){
@@ -107,15 +109,15 @@ const App = () => {
 							//add the difference to the players health
 							setPlayer1Health(player1Health + difference + player1Attack.heal);
 							//set the shield to 0
-							setPlayer1Defence(0);						
+							setPlayer1Shield(0);						
 						}else{
 							//subtract the damage from the shield
-							setPlayer1Defence(p1Shield - p1Update.damage);
+							setPlayer1Shield(p1Shield - p1Update.damage);
 						}
 
 					}else{
 						setPlayer1Health(player1Health - p1Update.damage + player1Attack.heal);
-						setPlayer1Defence(player1Defence + p1Update.shield);
+						setPlayer1Shield(player1Shield + p1Update.shield);
 					}
 
 					//Player 2
@@ -124,13 +126,13 @@ const App = () => {
 
 						if(difference < 0){
 							setPlayer2Health(player2Health + difference + player2Attack.heal);
-							setPlayer2Defence(0);
+							setPlayer2Shield(0);
 						}else{
-							setPlayer2Defence(p2Shield - p2Update.damage);
+							setPlayer2Shield(p2Shield - p2Update.damage);
 						}
 					}else{
 						setPlayer2Health(player2Health - p2Update.damage + player2Attack.heal);
-						setPlayer2Defence(player2Defence + p2Update.shield);
+						setPlayer2Shield(player2Shield + p2Update.shield);
 					}
 				
 				//update the state of p1 health and debuffs
@@ -252,7 +254,7 @@ const App = () => {
 			<div style={{display: 'flex', justifyContent: 'space-evenly'}}>
 				<h2>Player 1</h2>
 				<h2>Health: {player1Health}</h2>
-				<h2>Defence: {player1Defence}</h2>
+				<h2>Shield: {player1Shield}</h2>
 			</div>
 			<div style={{display: 'flex'}}>
 				{player1.map((spell, index) => {
@@ -266,7 +268,7 @@ const App = () => {
 			<div style={{display: 'flex', justifyContent: 'space-evenly'}}>
 				<h2>Player 2</h2>
 				<h2>Health: {player2Health}</h2>
-				<h2>Defence: {player2Defence}</h2>
+				<h2>Shield: {player2Shield}</h2>
 			</div>
 			<div style={{display: 'flex'}}>
 				{player2.map((spell, index) => {
