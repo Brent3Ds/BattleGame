20eed0b64e6953ee2b8c6edac6a26a26744c3907
Brian Ferguson <brianghferguson@gmail.com>
2021-03-23 21:24:17 -0400

    Added Health Bar
    Maximum health is currently hard coded

---
+++ golf/src/components/ProgressBar.js
@@ -0,0 +1,19 @@
+const container_styles = {
+    height: 20,
+    width: '100%',
+    background: '#888',
+    marginTop: 10
+}
+
+const progress_styles = {
+    height: '100%',
+    background: 'green'
+}
+
+const ProgressBar = ({ children, width }) => {
+    return <div style={container_styles}>
+        <div style={{...progress_styles, width: `${width}%`}}>{children}</div>
+    </div>
+}
+
+export default ProgressBar
