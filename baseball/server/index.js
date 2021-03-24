aad84bc02cae549e959d6ba0f94ca809ffdf6a2a
therjmurray <therjmurray@gmail.com>
2021-03-24 18:09:40 -0400

    websockets working

---
+++ baseball/server/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+ 
+app.use(function (req, res, next) {
+  console.log('middleware');
+  req.testing = 'testing';
+  return next();
+});
+ 
+app.get('/', function(req, res, next){
+  console.log('get route', req.testing);
+  res.end();
+});
+ 
+app.ws('/', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+  });
+
+  setInterval(() => {
+      ws.send('lol')
+    }, 1000)
+    
+  console.log('socket', req.testing);
+});
+ 
+app.listen(3000);
---
+++ baseball/src/App.js
@@ -0,0 +1,19 @@
+const socket = new WebSocket('ws://localhost:3000');
+
+// Connection opened
+// socket.addEventListener('open', function (event) {
+//     socket.send('Hello Server!');
+// });
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+});
+
+function App() {
+  return <div>
+    <button onClick={() => socket.send('Hello Server!')}>Crick</button>
+  </div>
+}
+
+export default App;
