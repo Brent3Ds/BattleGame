5ad949b0728506818cb2ea0eba4beb02e39ae0bb
therjmurray <therjmurray@gmail.com>
2021-03-23 21:03:33 -0400

    rename playerxAttack to playerxSpell

--- golf/src/App.js
+++ golf/src/App.js
@@ -11,18 +11,13 @@ const App = () => {
 	const [player1, setPlayer1] = useState([]);
 	const [player2, setPlayer2] = useState([]);
 	const [phase, setPhase] = useState("heroSelect");
-	const [player1Attack, setPlayer1Attack] = useState();
-	const [player2Attack, setPlayer2Attack] = useState();
-	// const [player1Defence, setPlayer1Defence] = useState(0);
-	// const [player2Defence, setPlayer2Defence] = useState(0);
-	// const [player1Hero.shield, setPlayer1Shield] = useState(0);
-	// const [player2Hero.shield, setPlayer2Shield] = useState(0);
+	const [player1Spell, setPlayer1Spell] = useState();
+	const [player2Spell, setPlayer2Spell] = useState();
 	const [player1Debuffs, setPlayer1Debuffs] = useState([])
 	const [player2Debuffs, setPlayer2Debuffs] = useState([])
 	const [player1Hero, setPlayer1Hero] = useState()
 	const [player2Hero, setPlayer2Hero] = useState()
 	const [result, setResult] = useState();
-	const [waitForDefence, setWaitForDefence] = useState();
 
 	//Check if ready to move from to next phase
 	useEffect(() => {
@@ -84,22 +79,22 @@ const App = () => {
 		let p1Shield = 0;
 		let p2Shield = 0;
 
-		if(player1Attack && player2Attack){
+		if(player1Spell && player2Spell){
 			//subtract attacks from players health
-			let p1Update = evaluateCombat(player1Attack, player2Attack, player1Debuffs, player2Debuffs);
-			let p2Update = evaluateCombat(player2Attack, player1Attack, player2Debuffs, player1Debuffs);
+			let p1Update = evaluateCombat(player1Spell, player2Spell, player1Debuffs, player2Debuffs);
+			let p2Update = evaluateCombat(player2Spell, player1Spell, player2Debuffs, player1Debuffs);
 
 			//check if either player or both players will die this turn
 			//Player Tie
-			if(player1Hero.health - p1Update.damage + player1Attack.heal <= 0 && player2Hero.health - p2Update.damage + player2Attack.heal <= 0){
+			if(player1Hero.health - p1Update.damage + player1Spell.heal <= 0 && player2Hero.health - p2Update.damage + player2Spell.heal <= 0){
 				setPhase("battleOver");
 				setResult(0);
 			//Player 2 Wins
-			}else if(player1Hero.health - p1Update.damage + player1Attack.heal <= 0 && player2Hero.health - p2Update.damage + player2Attack.heal >= 0){
+			}else if(player1Hero.health - p1Update.damage + player1Spell.heal <= 0 && player2Hero.health - p2Update.damage + player2Spell.heal >= 0){
 				setPhase("battleOver");
 				setResult(2);
 			//Player 1 Wins
-			}else if(player1Hero.health - p1Update.damage + player1Attack.heal >= 0 && player2Hero.health - p2Update.damage + player2Attack.heal <= 0){
+			}else if(player1Hero.health - p1Update.damage + player1Spell.heal >= 0 && player2Hero.health - p2Update.damage + player2Spell.heal <= 0){
 				setPhase("battleOver");
 				setResult(1);
 			}else{
@@ -114,7 +109,7 @@ const App = () => {
 
 						if(difference < 0){
 							//add the difference to the players health
-							setPlayer1Hero({...player1Hero, health: player1Hero.health + difference + player1Attack.heal})
+							setPlayer1Hero({...player1Hero, health: player1Hero.health + difference + player1Spell.heal})
 							//set the shield to 0
 							setPlayer1Hero({...setPlayer1Hero, shield: 0});						
 						}else{
@@ -123,7 +118,7 @@ const App = () => {
 						}
 
 					}else{
-						setPlayer1Hero({...player1Hero, health: player1Hero.health - p1Update.damage + player1Attack.heal})
+						setPlayer1Hero({...player1Hero, health: player1Hero.health - p1Update.damage + player1Spell.heal})
 						setPlayer1Hero(player1Hero.shield + p1Update.shield);
 					}
 
@@ -132,39 +127,39 @@ const App = () => {
 						let difference = p2Shield - p2Update.damage;
 
 						if(difference < 0){
-							setPlayer2Hero({...player2Hero, health: player2Hero.health + difference + player2Attack.heal, shield: 0})
+							setPlayer2Hero({...player2Hero, health: player2Hero.health + difference + player2Spell.heal, shield: 0})
 						}else{
 							setPlayer2Hero({...player2Hero, shield: p2Shield - p2Update.damage});
 						}
 					}else{
-						setPlayer2Hero({...player2Hero, health: player2Hero.health - p2Update.damage + player2Attack.heal, shield: player2Hero.shield + p2Update.shield})
+						setPlayer2Hero({...player2Hero, health: player2Hero.health - p2Update.damage + player2Spell.heal, shield: player2Hero.shield + p2Update.shield})
 					}
 				
 				//update the state of p1 health and debuffs
 				//delay to show the spells cast
 				setTimeout(function(){
 				//set player attacks to null
-				setPlayer1Attack(null);
-				setPlayer2Attack(null);
+				setPlayer1Spell(null);
+				setPlayer2Spell(null);
 				}, 3000);
 			}
 		}
 	//eslint-disable-next-line
-	}, [player1Attack, player2Attack])
+	}, [player1Spell, player2Spell])
 
 	//happens during battle phase
 	const castSpell = (spell) => {
 		//only call if phase is battle
-		if(phase === "battle" && (!player1Attack || !player2Attack)){
+		if(phase === "battle" && (!player1Spell || !player2Spell)){
 			//add the spell selected to the current players attack selection
 			if(turn === 1){
 				//set player 1 spell
-				setPlayer1Attack(spell);
+				setPlayer1Spell(spell);
 				//change the turn to player 2
 				setTurn(2);
 			}else{
 				//set player 2 spell
-				setPlayer2Attack(spell);
+				setPlayer2Spell(spell);
 				//set the turn to player 1
 				setTurn(1);
 			}
@@ -207,7 +202,7 @@ const App = () => {
 	}
 
 	const playerCard = (attack) => {
-		if (player1Attack && player2Attack) {
+		if (player1Spell && player2Spell) {
 			return <Card spell={attack} /> 
 		} else if (attack) {
 			return <div style={{
@@ -242,11 +237,11 @@ const App = () => {
 				return <div style={{display: "flex", flexGrow: 3, width: '100%', borderBottom: '2px solid #333', background: '#000', color: '#FFF'}}>
 					{/* Player 1 Selection */}
 					<div style={{width: '50%'}}>
-						{playerCard(player1Attack)}
+						{playerCard(player1Spell)}
 					</div>
 					{/* Player 2 Selection */}
 					<div style={{width: '50%'}}>
-						{playerCard(player2Attack)}
+						{playerCard(player2Spell)}
 					</div>
 				</div>
 			case 'battleOver':
