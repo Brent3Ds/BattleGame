411314164cbf34511407463771f5fa30e8e5426f
therjmurray <therjmurray@gmail.com>
2021-03-25 19:10:00 -0400

    display navigation component

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -1,3 +1,4 @@
+import Navigation from './components/Navigation'
 const socket = new WebSocket('ws://localhost:3001');
 
 // Connection opened
@@ -10,8 +11,8 @@ socket.addEventListener('message', function (event) {
     console.log('Message from server ', event.data);
 });
 const App = () => {
-  return <div>
-    <button onClick={() => socket.send('Hello Server!')}>Crick</button>
+  return <div style={{position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, background: '#F5F5F5'}}>
+    <Navigation />
   </div>
 }
 
