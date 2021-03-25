dbb4fe5964e0cde73ec3b8e3612d097c51d077d0
therjmurray <therjmurray@gmail.com>
2021-03-25 19:09:11 -0400

    create button component using emotion js

---
+++ baseball/src/components/Button.js
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled'
+
+export const Button = styled.button`
+  border: 0;
+  border-radius: 0.25rem;
+  background: #1E88E5;
+  color: white;
+  font-family: system-ui, sans-serif;
+  font-size: 1rem;
+  line-height: 1.2;
+  white-space: nowrap;
+  text-decoration: none;
+  padding: 0.25rem 1rem;
+  margin: 0.25rem;
+  cursor: pointer;
+  height: 40px;
+  user-select: none;
+`
